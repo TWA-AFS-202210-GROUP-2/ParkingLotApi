51d90d77129ea0e01b99ac3f9d5500d4fd1966da
Bo Zhao <BZhao7@slb.com>
2022-11-05 15:20:38 +0800

    feat: add Entity file in models

---
+++ ParkingLotApi/Models/OrderEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ParkingLotApi.Models
+{
+    public class OrderEntity
+    {
+        public int Id { get; set; }
+
+        public string ParkingLotName { get; set; }
+
+        public string PlateNumber { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public DateTime? CloseTime { get; set; }
+
+        public OrderStatus IsClose { get; set; } = OrderStatus.Open;
+    }
+
+    public enum OrderStatus
+    {
+        Open,
+        Closed,
+    }
+}
