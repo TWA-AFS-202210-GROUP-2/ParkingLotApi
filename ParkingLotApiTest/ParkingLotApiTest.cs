1d57c8ce68391c2917e66d530ac0930329484f8e
Bo Zhao <BZhao7@slb.com>
2022-11-06 10:10:27 +0800

    feat(AC3): can get all parkinglot

--- ParkingLotApiTest/ParkingLotApiTest.cs
+++ ParkingLotApiTest/ParkingLotApiTest.cs
@@ -11,6 +11,12 @@ namespace ParkingLotApiTest
 {
     public class ParkingLotApiTest : TestBase
     {
-        
+        [Fact]
+        public async void Should_return_parkinglot_with_orders_when_get_all()
+        {
+            NewParkingLotData();
+            var parkinglots = await _parkingLotService.GetAll();
+            Assert.Equal(2, parkinglots.Count);
+        }
     }
 }
