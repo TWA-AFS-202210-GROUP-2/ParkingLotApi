18618f74dfa89e4af82b738e7a9facbf27ae7459
Bo Zhao <BZhao7@slb.com>
2022-11-06 10:02:54 +0800

    test: initial test database setting and input data

---
+++ ParkingLotApiTest/ParkingLotApiTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingLotApi.Repository;
+using ParkingLotApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace ParkingLotApiTest
+{
+    public class ParkingLotApiTest : TestBase
+    {
+        
+    }
+}
